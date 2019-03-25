a8d22932267b0ab764aa2b9119e25900c8ba72d5
mselinbozkanat <48119991+mselinbozkanat@users.noreply.github.com>
2019-03-25 22:57:18 +0300

    ilk commit

---
+++ EF-DBFirst-SalesOrder/FormOrderHeaderDetail.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EF_DBFirst_SalesOrder
+{
+    public partial class FormOrderHeaderDetail : Form
+    {
+        public FormOrderHeaderDetail(int girilenID)
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrderUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrderDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertOrderDetail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdateOrderDetail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeleteOrderDetail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormOrderHeaderDetail_Load(object sender, EventArgs e)
+        {
+            NorthwindEntitiesConnectionString db = new NorthwindEntitiesConnectionString();
+            
+        }
+    }
+}
